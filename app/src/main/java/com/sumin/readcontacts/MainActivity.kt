8eab19f758c582011e1fe7be4835edbed4997898
SolomonVP <Lum7023@yandex.ru>
2024-11-03 20:51:42 +0300

    12.8 Получение разрешения от пользователя

--- app/src/main/java/com/sumin/readcontacts/MainActivity.kt
+++ app/src/main/java/com/sumin/readcontacts/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.sumin.readcontacts
 
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.provider.ContactsContract.Contacts
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlin.concurrent.thread
 
@@ -22,13 +21,32 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("MainActivity", "Permission denied")
+            requestPermission()
         }
+    }
 
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
 
-
-
-
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("MainActivity", "Permission Denied")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun requestContacts() {
@@ -40,13 +58,12 @@ class MainActivity : AppCompatActivity() {
                 null,
                 null
             )
-
             while (cursor?.moveToNext() == true) {
                 val id = cursor.getInt(
                     cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
                 )
                 val name = cursor.getString(
-                    cursor.getColumnIndexOrThrow(Contacts.DISPLAY_NAME)
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
                 )
                 val contact = Contact(id, name)
                 Log.d("MainActivity", contact.toString())
@@ -55,4 +72,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    companion object {
+
+        private const val READ_CONTACTS_RC = 100
+    }
 }
